794161e03170a1a807a3ffe9ab3e61adb0837d9b
secondLieutenantCoder <xxxwangwei@163.com>
2016-10-22 13:24:28 +0800

    Create text.m
    添加了一个.m文件。

---
+++ text.m
@@ -0,0 +1,15 @@
+ #import "text.h"
+ 
+ @interface text :NSObject
+ 
+ end
+ 
+ @implention text
+ 
+ - (void) initWithName:(NSString *)name andFrame:(CGRect )frame{
+ 
+ NSLog(@"自定义的初始化方法==============！");
+ 
+ }
+ 
+ end
